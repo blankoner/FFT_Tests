855bcfa21391d4b8f41d14f5cbe9819d0f1da715
Marcel Tracz <traczmarcel@gmail.com>
2023-12-01 22:22:00 +0100

    Przebudowa programu na wersje obiektowa

--- main.cpp
+++ main.cpp
@@ -1,158 +1,196 @@
 #include <iostream>
 #include <complex>
 #include <cmath>
-#include <fftw3.h>
 
-void Zero(std::complex<double> *C, int N_in)
+class Transformata
 {
-  for(int i=0;i<N_in;i++) C[i] = std::complex<double>(0.0, 0.0);
+  private:
+    Transformata(const Transformata& s);//zabezpieczenie konstruktora (konstr. kopiujacym) i nizej operatora przypisania
+    Transformata& operator=(const Transformata& s);
+    std::complex<double> *fi, *FFTfi, *C, *IDFTfi, *IFFTfi;
+    std::complex<double> **fi2d;
+    int licznikDFT, licznikFFT, wymiar, N, M;
+    double *DFTmodules, *FFTmodules;
+  public:
+    Transformata();
+    ~Transformata();
+    void Set();
+    void Zero(std::complex<double> *tab, int r);
+    void Build1D();
+    void Build2D();
+    void DFT();
+    void IDFT();
+    void FFT(std::complex<double>* data, int size);
+    double CountModule(double re, double im){return sqrt(pow(re, 2) + pow(im, 2));}
+    void Get1D();
+    void Get2D();
+    void Start();
+};
+
+Transformata::Transformata()
+{
+  licznikDFT = 0;
+  licznikFFT = 0;
+  wymiar = 0;
+  N = 0;
+  M = 0;
 }
 
-double CountModule(double Re, double Im)
+void Transformata::Set()
 {
-  return sqrt(pow(Re, 2) + pow(Im, 2));
+  std::cin >> wymiar;
+  if(wymiar == 1) std::cin >> N;
+  else std::cin >> N >> M;
 }
 
-void DFT(std::complex<double> *C, double *f, int N_in, double *Mods)
+void Transformata::Zero(std::complex<double> *tab, int r)
 {
-  int licznik = 0;
-  std::complex<double> j(0.0, 1.0);
-  for(int n=0;n<N_in;n++)
+  for(int i=0;i<r;i++) tab[i] = 0.0;
+}
+
+void Transformata::Build1D()
+{
+  fi = new std::complex<double>[N]; FFTfi = new std::complex<double>[N]; C = new std::complex<double>[N];
+  IDFTfi = new std::complex<double>[N]; IFFTfi = new std::complex<double>[N];
+  DFTmodules = new double[N]; FFTmodules = new double[N];
+  for(int i=0;i<N;i++)
   {
-    for(int i=0;i<N_in;i++)
+    std::cin >> fi[i];
+    FFTfi[i] = fi[i];
+  }
+}
+
+void Transformata::Build2D()
+{
+  fi2d = new std::complex<double>*[N];
+  for(int i=0;i<N;i++){fi2d[i] = new std::complex<double>[M];}
+  for(int i=0;i<N;i++)
+  {
+    for(int j=0;j<M;j++) std::cin >> fi2d[i][j];
+  }
+}
+
+void Transformata::DFT()
+{
+  Zero(C, N);
+  std::complex<double> j(0.0, 1.0);
+  for(int n=0;n<N;n++)
+  {
+    for(int i=0;i<N;i++)
     {
-      C[n] += std::complex<double>(f[i], 0.0) * exp((-j * std::complex<double>((2*M_PI*i*n), 0.0))/static_cast<double>(N_in));
-      licznik++;
+      C[n] += fi[i] * exp((-j * std::complex<double>((2*M_PI*i*n), 0.0))/static_cast<double>(N));
+      licznikDFT++;
     }
     if(fabs(C[n].real()) < 5e-4) C[n].real(0.0);
     if(fabs(C[n].imag()) < 5e-4) C[n].imag(0.0);
-    Mods[n] = CountModule(C[n].real(), C[n].imag());
-  }
-  std::cout << "Licznik DFT: " << licznik << std::endl;
-}
-
-void IDFT(std::complex<double> *f, std::complex<double> *C, int N_in)
-{
-  std::complex<double> j(0.0, 1.0);
-  for(int i=0;i<N_in;i++)
-  {
-    for(int n=0;n<N_in;n++)
-    {
-      f[i] += C[n] * exp((j * std::complex<double>((2*M_PI*i*n), 0.0))/static_cast<double>(N_in));
-    }
-    f[i] *= (1/static_cast<double>(N_in));
+    DFTmodules[n] = CountModule(C[n].real(), C[n].imag());
   }
 }
 
-void FFT(std::complex<double> *C, double *f, int N_in, double *Mods)
+void Transformata::IDFT()
 {
-  int licznik = 0;
   std::complex<double> j(0.0, 1.0);
-  for(int n=0;n<(N_in/2);n++)
+  for(int i=0;i<N;i++)
   {
-    for(int i=0;i<(N_in/2);i++)
+    IDFTfi[i] = std::complex<double>(0.0, 0.0);
+    for(int n=0;n<N;n++)
     {
-      C[n] += std::complex<double>(f[2*i], 0.0) * exp(-j * std::complex<double>((2*M_PI*i*n), 0.0)/(static_cast<double>(N_in)/2)) + exp(-j * std::complex<double>((2*M_PI*n), 0.0)/static_cast<double>(N_in)) * std::complex<double>(f[2*i+1], 0.0) * exp(-j * std::complex<double>((2*M_PI*i*n),0.0)/(static_cast<double>(N_in)/2));
-      C[n + (N_in/2)] += std::complex<double>(f[2*i], 0.0) * exp(-j * std::complex<double>((2*M_PI*i*n), 0.0)/(static_cast<double>(N_in)/2)) - exp(-j * std::complex<double>((2*M_PI*n), 0.0)/static_cast<double>(N_in)) * std::complex<double>(f[2*i+1], 0.0) * exp(-j * std::complex<double>((2*M_PI*i*n),0.0)/(static_cast<double>(N_in)/2));
-      licznik++;
+      IDFTfi[i] += C[n] * exp((j * std::complex<double>((2*M_PI*i*n), 0.0))/static_cast<double>(N));
     }
+    IDFTfi[i] *= (1/static_cast<double>(N));
   }
-  for(int n=0;n<N_in;n++)
-  {
-    if(fabs(C[n].real()) < 5e-4) C[n].real(0.0);
-    if(fabs(C[n].imag()) < 5e-4) C[n].imag(0.0);
-    Mods[n] = CountModule(C[n].real(), C[n].imag());
-  }
-  std::cout << "Licznik FFT: " << licznik << std::endl;
 }
 
-void IFFT(std::complex<double> *f, std::complex<double> *C, int N_in)
+void Transformata::FFT(std::complex<double>* data, int size)
 {
-  std::complex<double> j(0.0, 1.0);
-  for(int i=0;i<N_in/2;i++)
+  if(size <= 1) return;
+
+  std::complex<double>* even = new std::complex<double>[size / 2];
+  std::complex<double>* odd = new std::complex<double>[size / 2];
+
+  for (int i=0;i<size/2;++i)
   {
-    for(int n=0;n<N_in/2;n++)
+    even[i] = data[i*2];
+    odd[i] = data[i*2+1];
+  }
+
+  FFT(even, size/2);
+  FFT(odd, size/2);
+
+  for(int i=0;i<size/2;++i)
+  {
+    std::complex<double> t = std::polar(1.0, -2.0 * M_PI * i / size) * odd[i];
+    data[i] = even[i] + t;
+    data[i+size/2] = even[i] - t;
+    licznikFFT+=2;
+  }
+
+  delete[]even;
+  delete[]odd;
+}
+
+void Transformata::Get1D()
+{
+  std::cout << "Zlozonosc DFT: " << licznikDFT << std::endl;
+  std::cout << "Zlozonosc FFT: " << licznikFFT << std::endl;
+  for(int i=0;i<N;i++)
+  {
+    if(fabs(FFTfi[i].real()) < 0.0005) FFTfi[i].real(0.0);
+    if(fabs(FFTfi[i].imag()) < 0.0005) FFTfi[i].imag(0.0);
+    FFTmodules[i] = CountModule(FFTfi[i].real(), FFTfi[i].imag());
+  }
+  for(int i=0;i<N;i++) std::cout << i << " " << fi[i].real() << " " << DFTmodules[i] << " " << IDFTfi[i].real() << " " << FFTmodules[i] << std::endl;
+}
+
+void Transformata::Get2D()
+{
+  for(int i=0;i<N;i++)
+  {
+    for(int j=0;j<M;j++)
     {
-      f[i] += (1/static_cast<double>(N_in)) * ( C[2*n] * exp(j * std::complex<double>((2*M_PI*i*n), 0.0)/(static_cast<double>(N_in)/2)) + exp(j * std::complex<double>((2*M_PI*n), 0.0)/static_cast<double>(N_in)) * C[2*n+1] * exp(j * std::complex<double>((2*M_PI*i*n), 0.0)/(static_cast<double>(N_in)/2)) );
-      f[i+(N_in/2)] += (1/static_cast<double>(N_in)) * ( C[2*n]* exp(j * std::complex<double>((2*M_PI*i*n), 0.0)/(static_cast<double>(N_in)/2)) - exp(j * std::complex<double>((2 * M_PI * n),0.0)/static_cast<double>(N_in)) * C[2*n+1] * exp(j * std::complex<double>((2*M_PI*i*n),0.0)/(static_cast<double>(N_in)/2)) );
+      std::cout << fi2d[i][j] << " ";
     }
+    std::cout << std::endl;
+  }
+}
+
+void Transformata::Start()
+{
+  Set();
+  if(wymiar == 1)
+  {
+    Build1D();
+    DFT();
+    IDFT();
+    FFT(FFTfi, N);
+    Get1D();
+  }
+  else
+  {
+    Build2D();
+    Get2D();
+  }
+}
+
+Transformata::~Transformata()
+{
+  if(fi) delete[]fi;
+  if(FFTfi) delete[]FFTfi;
+  if(IDFTfi) delete[]IDFTfi;
+  if(IFFTfi) delete[]IFFTfi;
+  if(C) delete[]C;
+  if(DFTmodules) delete[]DFTmodules;
+  if(FFTmodules) delete[]FFTmodules;
+  if(fi2d)
+  {
+    for(int i=0;i<N;i++) delete[]fi2d[i];
+    delete[]fi2d;
   }
 }
 
 int main()
 {
-  int wymiar = 0;
-  int N = 0, M = 0;
-  double *fi;
-  double *DFTmodules;
-  double *FFTmodules;
-  std::complex<double> *Cn, *IDFTfi, *IFFTfi;
-  double **fi2d;
-  
-  std::cin >> wymiar;
-  if(wymiar == 1)
-  {
-    std::cin >> N;
-    fi = new double[N];
-    Cn = new std::complex<double>[N], IDFTfi = new std::complex<double>[N], IFFTfi = new std::complex<double>[N];
-    DFTmodules = new double[N];
-    FFTmodules = new double[N];
-    for(int i=0;i<N;i++) std::cin >> fi[i];
-    DFT(Cn, fi, N, DFTmodules);
-    IDFT(IDFTfi, Cn, N);
-    Zero(Cn, N);
-    FFT(Cn, fi, N, FFTmodules);
-    IFFT(IFFTfi, Cn, N);
-    for(int i=0;i<N;i++) std::cout << i << " " << DFTmodules[i] << " " << IDFTfi[i].real() << " " << FFTmodules[i] << " " << IFFTfi[i].real() << std::endl;
-  }
-  else
-  {
-    std::cin >> N >> M;
-    fi2d = new double*[N];
-    for(int i=0;i<N;i++) fi2d[i] = new double[M];
-    for(int i=0;i<N;i++)
-    {
-      for(int j=0;j<M;j++) std::cin >> fi2d[i][j];
-    }
-
-    // Allocate memory for FFTW plans and output data
-    fftw_complex* in = reinterpret_cast<fftw_complex*>(fi2d);
-    fftw_complex* out = reinterpret_cast<fftw_complex*>(fftw_malloc(sizeof(fftw_complex) * N * M));
-
-    // Create FFTW plans
-    fftw_plan plan = fftw_plan_dft_2d(N, M, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-
-    // Execute the FFT
-    fftw_execute(plan);
-
-    // Output the result as complex numbers and their modulus
-    /*for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            std::cout << std::fixed << "(" << out[i * N + j][0] << " + " << out[i * N + j][1] << "i)   ";
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl << std::endl << std::endl;*/
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            double modulus = sqrt(pow(out[i * N + j][0], 2) + pow(out[i * N + j][1], 2));
-            std::cout << std::fixed << modulus << "   ";
-        }
-        std::cout << std::endl;
-    }
-
-    // Clean up
-    fftw_destroy_plan(plan);
-    fftw_free(out);
-  }
-  
-  delete[]fi;
-  delete[]IDFTfi;
-  delete[]IFFTfi;
-  delete[]Cn;
-  delete[]DFTmodules;
-  delete[]FFTmodules;
-  for(int i=0;i<M;i++) delete[]fi2d[i];
-  delete[]fi2d;
+  Transformata T;
+  T.Start();
   return 0;
 }
